ba6a69a35c00e07d34dfd165664f3c9249d332d0
Yu Li <ylilarry@gmail.com>
2023-03-21 03:11:10 -0400

    [Misc] Add CLI version check. Change site title

--- cli/fastchargeapi_cli/groups.py
+++ cli/fastchargeapi_cli/groups.py
@@ -1,14 +1,15 @@
 import click
 from click_aliases import ClickAliasedGroup
+from .version import check_version_or_exit
 
 
 @click.group(cls=ClickAliasedGroup)  # type: ignore
 @click.help_option("-h", "--help")
 def fastapi():
-    pass
+    check_version_or_exit()
 
 
 @click.group(cls=ClickAliasedGroup)
 @click.help_option("-h", "--help")
 def fastcharge():
-    pass
+    check_version_or_exit()
