5e18828c912b804537d097a189f15e9a327f6bbb
Yu Li <ylilarry@gmail.com>
2023-06-02 02:28:53 -0400

    [GraphQL] Split SQSQueueUrl to dev and prod

--- graphql-service/apollo/cron-jobs/sqsClient.ts
+++ graphql-service/apollo/cron-jobs/sqsClient.ts
@@ -1,28 +1,21 @@
 import { InMemoryCache } from "@apollo/client/cache/inmemory/inMemoryCache";
 import { ApolloClient } from "@apollo/client/core/ApolloClient";
 import { HttpLink } from "@apollo/client/link/http/HttpLink";
-import { v4 as uuidv4 } from "uuid";
 import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { RequestInit, Response } from "node-fetch";
+import { v4 as uuidv4 } from "uuid";
+import { awsAccountId } from "../runtime-config";
 
 const cache = new InMemoryCache();
 
-export enum SQSQueueUrl {
-    BillingFifoQueue = "https://sqs.us-east-1.amazonaws.com/887279901853/graphql-service-billing-queue.fifo",
-    UsageLogQueue = "https://sqs.us-east-1.amazonaws.com/887279901853/graphql-service-usage-log-queue",
-}
+export const SQSQueueUrl = {
+    BillingFifoQueue: `https://sqs.us-east-1.amazonaws.com/${awsAccountId}/graphql-service-billing-queue.fifo`,
+    UsageLogQueue: `https://sqs.us-east-1.amazonaws.com/${awsAccountId}/graphql-service-usage-log-queue`,
+};
 
 const sqsClient = new SQSClient({ region: "us-east-1" });
 
-export function sqsGQLClient({
-    queueUrl,
-    dedupId,
-    groupId,
-}: {
-    queueUrl: SQSQueueUrl;
-    dedupId?: string;
-    groupId?: string;
-}) {
+export function sqsGQLClient({ queueUrl, dedupId, groupId }: { queueUrl: string; dedupId?: string; groupId?: string }) {
     return new ApolloClient({
         cache: cache,
         // Disabling cache will prevent error because we can't return a response
