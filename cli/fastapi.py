f9bc1f8ac1b19bfe0064223eedbc6975f11c8500
Yu Li <ylilarry@gmail.com>
2023-06-16 17:51:59 -0400

    [CLI] Move cli code to a git submodule

--- cli/fastapi.py
+++
@@ -1,5 +0,0 @@
-#!python
-from fastchargeapi_cli.main import fastapi
-
-if __name__ == "__main__":
-    fastapi()
--- cli/fastcharge.py
+++
@@ -1,5 +0,0 @@
-#!python
-from fastchargeapi_cli.main import fastcharge
-
-if __name__ == "__main__":
-    fastcharge()
