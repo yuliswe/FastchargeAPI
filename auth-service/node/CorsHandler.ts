41b79e3f08d5f67f6453916e1800b9dd6784f47c
Yu Li <contact+github@yuli.se>
2024-01-02 02:46:07 -0500

    Update auth-service for new APIs

--- auth-service/node/CorsHandler.ts
+++ auth-service/node/CorsHandler.ts
@@ -2,44 +2,40 @@
  * You can use this file as a template for your Lambda function.
  */
 
-import { Context as LambdaContext, APIGatewayProxyStructuredResultV2 } from "aws-lambda";
-import { Chalk } from "chalk";
-import { LambdaCallbackV2, LambdaEventV2, LambdaHandlerV2 } from "../utils/LambdaContext";
-
-const chalk = new Chalk({ level: 3 });
+import type { APIGatewayProxyStructuredResultV2 } from "aws-lambda";
+import chalk from "chalk";
+import type { LambdaEventV2, LambdaHandlerV2 } from "../utils/LambdaContext";
 
 async function handle(event: LambdaEventV2): Promise<APIGatewayProxyStructuredResultV2> {
-    if (event.requestContext.http.method === "OPTIONS") {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({}),
-        };
-    } else {
-        return Promise.resolve({
-            statusCode: 401,
-            body: JSON.stringify({}),
-        });
-    }
+  if (event.requestContext.http.method === "OPTIONS") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({}),
+    };
+  } else {
+    return Promise.resolve({
+      statusCode: 401,
+      body: JSON.stringify({}),
+    });
+  }
 }
 
 export const lambdaHandler: LambdaHandlerV2 = async (
-    event: LambdaEventV2,
-    context: LambdaContext,
-    callback: LambdaCallbackV2
+  event: LambdaEventV2
 ): Promise<APIGatewayProxyStructuredResultV2> => {
+  try {
+    console.log("event", chalk.blue(JSON.stringify(event)));
+    return await handle(event);
+  } catch (error) {
     try {
-        console.log("event", chalk.blue(JSON.stringify(event)));
-        return await handle(event);
-    } catch (error) {
-        try {
-            console.error(error);
-            console.error(chalk.red(JSON.stringify(error)));
-        } catch (jsonError) {
-            console.error(error);
-        }
-        return {
-            statusCode: 500,
-            body: "Internal Server Error",
-        };
+      console.error(error);
+      console.error(chalk.red(JSON.stringify(error)));
+    } catch (jsonError) {
+      console.error(error);
     }
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+  }
 };
--- auth-service/node/RefreshIdToken.ts
+++ auth-service/node/RefreshIdToken.ts
@@ -1,82 +1,86 @@
+import { getParameterFromAWSSystemsManager } from "@/functions/aws";
 import { APIGatewayProxyStructuredResultV2, Context as LambdaContext } from "aws-lambda";
 import { Chalk } from "chalk";
-import { getParameterFromAWSSystemsManager } from "graphql-service/functions/aws";
 import fetch from "node-fetch";
 import { LambdaCallbackV2, LambdaEventV2, LambdaHandlerV2 } from "../utils/LambdaContext";
 
 const chalk = new Chalk({ level: 3 });
 
 type RefreshTokenResponse = {
-    access_token: string;
-    expires_in: string;
-    token_type: string;
-    refresh_token: string;
-    id_token: string;
-    user_id: string;
-    project_id: string;
+  access_token: string;
+  expires_in: string;
+  token_type: string;
+  refresh_token: string;
+  id_token: string;
+  user_id: string;
+  project_id: string;
+};
+
+type RequestBody = {
+  refreshToken?: string;
 };
 
 let API_KEY: string | undefined;
 async function handle(event: LambdaEventV2): Promise<APIGatewayProxyStructuredResultV2> {
-    let refreshToken: string;
-    try {
-        refreshToken = JSON.parse(event.body ?? "{}").refreshToken;
-    } catch (error) {
-        return {
-            statusCode: 400,
-            body: "Invalid Request: Body is not JSON",
-        };
-    }
-    if (!refreshToken) {
-        return {
-            statusCode: 400,
-            body: "Required in body: { refreshToken: string }",
-        };
-    }
-    if (!API_KEY) {
-        API_KEY = await getParameterFromAWSSystemsManager("auth.google_api_key.for_refresh_token");
-    }
-    const response = await fetch(`https://securetoken.googleapis.com/v1/token?key=${API_KEY}`, {
-        method: "POST",
-        body: JSON.stringify({
-            grant_type: "refresh_token",
-            refresh_token: refreshToken,
-        }),
-    });
-    if (!response.ok) {
-        return {
-            statusCode: 400,
-            body: "Invalid Request: Refresh token is invalid.",
-        };
-    }
-    const data = (await response.json()) as RefreshTokenResponse;
+  let refreshToken: string | undefined;
+  try {
+    ({ refreshToken } = JSON.parse(event.body ?? "{}") as RequestBody);
+  } catch (error) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({
-            idToken: data.id_token,
-            refreshToken: data.refresh_token,
-        }),
+      statusCode: 400,
+      body: "Invalid Request: Body is not JSON",
     };
+  }
+  if (!refreshToken) {
+    return {
+      statusCode: 400,
+      body: "Required in body: { refreshToken: string }",
+    };
+  }
+  if (!API_KEY) {
+    API_KEY = await getParameterFromAWSSystemsManager("auth.google_api_key.for_refresh_token");
+  }
+  const response = await fetch(`https://securetoken.googleapis.com/v1/token?key=${API_KEY}`, {
+    method: "POST",
+    body: JSON.stringify({
+      grant_type: "refresh_token",
+      refresh_token: refreshToken,
+    }),
+  });
+  if (!response.ok) {
+    return {
+      statusCode: 400,
+      body: "Invalid Request: Refresh token is invalid.",
+    };
+  }
+  const data = (await response.json()) as RefreshTokenResponse;
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      idToken: data.id_token,
+      refreshToken: data.refresh_token,
+    }),
+  };
 }
 
 export const lambdaHandler: LambdaHandlerV2 = async (
-    event: LambdaEventV2,
-    context: LambdaContext,
-    callback: LambdaCallbackV2
+  event: LambdaEventV2,
+  context: LambdaContext,
+  callback: LambdaCallbackV2
 ): Promise<APIGatewayProxyStructuredResultV2> => {
+  try {
+    console.log("event", chalk.blue(JSON.stringify(event)));
+    return await handle(event);
+  } catch (error) {
     try {
-        console.log("event", chalk.blue(JSON.stringify(event)));
-        return await handle(event);
-    } catch (error) {
-        try {
-            console.error(error);
-            console.error(chalk.red(JSON.stringify(error)));
-        } catch (jsonError) {
-            console.error(error);
-        }
-        return {
-            statusCode: 500,
-            body: "Internal Server Error",
-        };
+      console.error(error);
+      console.error(chalk.red(JSON.stringify(error)));
+    } catch (jsonError) {
+      console.error(error);
     }
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+  }
 };
