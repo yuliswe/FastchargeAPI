35aa07690e20d1cd668499dec1e9dd8d8edaea0c
Yu Li <contact+github@yuli.se>
2024-01-20 00:37:58 -0500

    Fix HomePageBanner responsiveness

--- react/fastchargeapi/src/stateless-components/HomePageBanner2.tsx
+++ react/fastchargeapi/src/stateless-components/HomePageBanner2.tsx
@@ -17,141 +17,179 @@ export class HomePageBanner2 extends React.PureComponent {
   get _context() {
     return this.context as AppContext;
   }
-  render() {
+
+  renderRightBox() {
     return (
-      <Box // background
-        sx={{
-          bgcolor: "primary.light",
-          position: "relative",
-          overflow: "hidden",
-          p: this._context.mediaQuery.md.down ? 5 : 10,
-        }}
-      >
-        <Container
-          maxWidth="lg"
+      <Box sx={{ position: "relative", flexGrow: 1 }}>
+        <Box
+          component={BannerIconServer}
           sx={{
-            inset: 0,
+            left: "20%",
+            top: "15%",
+            height: 200,
+            width: 200,
             position: "absolute",
-            display: this._context.mediaQuery.md.down ? "none" : "inherit",
+            color: "primary.light",
           }}
-        >
-          <Box
-            component={BannerIconServer}
-            sx={{
-              right: 25,
-              top: 100,
-              height: 200,
-              width: 200,
-              position: "absolute",
-              color: "primary.light",
-            }}
-          />
-          <Box
-            component={BannerIconScribbleShape}
-            sx={{
-              right: 280,
-              top: 130,
-              height: 30,
-              width: 30,
-              position: "absolute",
-              color: "primary.main",
-            }}
-          />
+        />
+        {this._context.mediaQuery.xl.up && (
           <Box
             component={BannerIconLaptop}
             sx={{
-              left: 900,
-              bottom: 100,
+              left: 0,
+              top: "60%",
               height: 150,
               width: 150,
               position: "absolute",
               color: "primary.dark",
             }}
           />
-          <Box
-            component={BannerIconScribbleCloud}
-            sx={{
-              left: 1100,
-              bottom: 150,
-              height: 50,
-              width: 50,
-              position: "absolute",
-              color: "primary.main",
-            }}
-          />
+        )}
+        <Box
+          component={BannerIconScribbleCloud}
+          sx={{
+            left: "70%",
+            top: "70%",
+            height: 50,
+            width: 50,
+            position: "absolute",
+            color: "primary.main",
+          }}
+        />
+        {this._context.mediaQuery.xl.up && (
           <Box
             component={BannerIconScribbleArrow2}
             color="primary.secondary"
             sx={{
-              left: 1200,
+              left: "80%",
               bottom: 250,
               height: 50,
               width: 50,
               position: "absolute",
             }}
           />
-          <Box
-            component={BannerIconScribbleCircle}
-            sx={{
-              left: 200,
-              top: 70,
-              height: 25,
-              width: 25,
-              position: "absolute",
-              color: "primary.main",
-            }}
-          />
+        )}
+        <Box
+          component={BannerIconScribbleShape}
+          sx={{
+            left: "10%",
+            top: "10%",
+            height: 30,
+            width: 30,
+            position: "absolute",
+            color: "primary.main",
+          }}
+        />
+      </Box>
+    );
+  }
+
+  renderLeftBox() {
+    return (
+      <Box sx={{ position: "relative", flexGrow: 1 }}>
+        <Box
+          component={BannerIconScribbleCircle}
+          sx={{
+            right: "10%",
+            top: "10%",
+            height: 25,
+            width: 25,
+            position: "absolute",
+            color: "primary.main",
+          }}
+        />
+        {this._context.mediaQuery.xl.up && (
           <Box
             component={BannerIconAddFiles}
             sx={{
-              left: 25,
-              top: 150,
-              height: 125,
-              width: 125,
+              right: "60%",
+              top: "15%",
+              height: 100,
+              width: 100,
               position: "absolute",
               color: "primary.dark",
             }}
           />
-          <Box
-            component={BannerIconScribbleArrow}
-            sx={{ left: 200, top: 150, height: 70, width: 70, position: "absolute" }}
-          />
-          <Box
-            component={BannerIconCoding}
-            sx={{
-              right: "min(950px, 80%)",
-              bottom: 100,
-              height: 200,
-              width: 200,
-              position: "absolute",
-              color: "primary.dark",
-            }}
-          />
+        )}
+        <Box
+          component={BannerIconScribbleArrow}
+          sx={{ right: "10%", top: "25%", height: 70, width: 70, position: "absolute" }}
+        />
+        <Box
+          component={BannerIconCoding}
+          sx={{
+            right: "5%",
+            top: "45%",
+            height: 200,
+            width: 200,
+            position: "absolute",
+            color: "primary.dark",
+          }}
+        />
+        {this._context.mediaQuery.xl.up && (
           <Box
             component={BannerIconScribbleDoubleCircle}
             sx={{
               color: "primary.main",
-              right: "min(1200px, 110%)",
+              top: "80%",
+              right: "80%",
               bottom: 100,
               height: 30,
               width: 30,
               position: "absolute",
             }}
           />
+        )}
+      </Box>
+    );
+  }
+
+  renderCenterBox() {
+    return (
+      <Stack spacing={5} py={10} mx={10} alignItems="center" display="flex" zIndex={2}>
+        <Typography variant="h1" maxWidth="10em" textAlign="center">
+          Create revenue from your{" "}
+          <Box component="span" color="primary.main" sx={{ fontWeight: 425 }}>
+            APIs
+          </Box>
+          .
+        </Typography>
+        <Typography variant="body1" textAlign="center">
+          Publish your SaaS APIs here and start generating revenue from millions of developers.
+        </Typography>
+        <AppSearchBar />
+      </Stack>
+    );
+  }
+
+  render() {
+    return (
+      <Box // background
+        sx={{
+          position: "relative",
+          bgcolor: "primary.light",
+          overflow: "hidden",
+        }}
+      >
+        <Container
+          maxWidth="xl"
+          sx={{
+            inset: 0,
+            display: this._context.mediaQuery.md.down ? "none" : "inherit",
+          }}
+        >
+          <Stack
+            direction="row"
+            sx={{
+              alignItems: "stretch",
+              justifyContent: "space-around",
+            }}
+          >
+            {this._context.mediaQuery.lg.up && this.renderLeftBox()}
+            {this.renderCenterBox()}
+            {this._context.mediaQuery.lg.up && this.renderRightBox()}
+          </Stack>
         </Container>
-        <Stack spacing={5} py={5} alignItems="center" display="flex" zIndex={2}>
-          <Typography variant="h1" maxWidth="10em" textAlign="center">
-            Create revenue from your{" "}
-            <Box component="span" color="primary.main" sx={{ fontWeight: 425 }}>
-              APIs
-            </Box>
-            .
-          </Typography>
-          <Typography variant="body1" textAlign="center">
-            Publish your SaaS APIs here and start generating revenue from millions of developers.
-          </Typography>
-          <AppSearchBar />
-        </Stack>
       </Box>
     );
   }
