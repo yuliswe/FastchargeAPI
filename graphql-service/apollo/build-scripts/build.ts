6e1002afd179c219b66e881ff00353cde3f45cbe
Yu Li <contact+github@yuli.se>
2024-03-03 12:22:54 -0500

    Fix build script

--- graphql-service/apollo/build-scripts/build.ts
+++ graphql-service/apollo/build-scripts/build.ts
@@ -1,13 +1,14 @@
+import type { BuildOptions } from "esbuild";
 import * as esbuild from "esbuild";
 
-esbuild.buildSync({
-  entryPoints: ["./src/lambdaHandler.ts", "./src/sqsHandler.ts", "devserver.ts"],
+const buildOptions: BuildOptions = {
   bundle: true,
   outdir: "./dist",
   platform: "node",
   target: "node18",
   format: "cjs",
   sourcemap: true,
+  minify: true,
   loader: {
     ".graphql": "text",
     ".hbs": "text",
@@ -15,4 +16,15 @@ esbuild.buildSync({
   outExtension: {
     ".js": ".cjs",
   },
+};
+
+esbuild.buildSync({
+  entryPoints: ["devserver.ts"],
+  ...buildOptions,
+});
+
+esbuild.buildSync({
+  entryPoints: ["./src/lambdaHandler.ts", "./src/sqsHandler.ts"],
+  outbase: "./src",
+  ...buildOptions,
 });
